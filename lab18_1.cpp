e120242eea22a20b7313e95416ade1fce5d14bb8
PhichaPrim <phichamon_duangmanee@cmu.ac.th>
2024-02-13 13:16:33 +0700

    lab18_1.cpp completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,14 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-struct Rect{
-	double x,y,w,h;
+struct Rect {
+    double x, y, w, h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect R1, Rect R2) {
+    double x = min((R1.x+R1.w),(R2.x+R2.w))-max(R1.x,R2.x);
+    double y = min(R1.y,R2.y)-max((R1.y-R1.h),(R2.y-R2.h)); 
+    if(x<=0 || y<=0) return 0;
+    return x*y;
 }
